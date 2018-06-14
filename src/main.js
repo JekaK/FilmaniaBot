6c56adc01384b16f8df468e1443b32550f5749da
JekaK <krukun1998@gmail.com>
2018-06-14 12:09:31 +0300

    Already done

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const bot = new Telegraf(botToken);
 const LocalSession = require('telegraf-session-local');
 const request = require('request');
 const util = require('../Util/Util');
-
+const fetch = require('node-fetch')
 setInterval(() => {
     bot.telegram.getWebhookInfo()
         .then((res) => {
@@ -21,55 +21,84 @@ setInterval(() => {
 }, 2000);
 
 
-function fetchData(query, cb) {
-    request({
-        method: 'GET',
-        url: `http://api.themoviedb.org/3/search/movie?api_key=${tokenDb}&query=${query}&page=${1}`,
-        headers: [
-            {
-                name: 'content-type',
-                value: 'application/json;charset=utf-8'
-            }
-        ]
-    }, (err, resp, body) => {
-        let b = JSON.parse(body);
-        util.genreIdChecker(b);
-        cb(b)
-    })
+async function fetchData(query, cb) {
+    /*  request({
+          method: 'GET',
+          url: `http://api.themoviedb.org/3/search/movie?api_key=${tokenDb}&query=${query}&page=${1}`,
+          headers: [
+              {
+                  name: 'content-type',
+                  value: 'application/json;charset=utf-8'
+              }
+          ]
+      }, (err, resp, body) => {
+          let b = JSON.parse(body);
+          util.genreIdChecker(b);
+          cb(b)
+      })*/
+    const apiUrl = `http://api.themoviedb.org/3/search/movie?api_key=${tokenDb}&query=${query}&page=${1}`
+    const response = await fetch(apiUrl);
+    const body = await response.json();
+    util.genreIdChecker(body);
+    return body
 }
-bot.command('/start',ctx=>{
+
+bot.command('/start', ctx => {
     ctx.reply('Hello')
 });
 
-bot.on('inline_query', (ctx) => {
-    if (ctx.inlineQuery.query !== '')
-        fetchData(ctx.inlineQuery.query, (b) => {
-            const results = b.results.map((movie) => {
-                return {
-                    type: 'article',
-                    id: movie.id,
-                    title: movie.title,
-                    description: movie.overview,
-                    thumb_url: `http://image.tmdb.org/t/p/original${movie.poster_path}`,
-                    input_message_content: {
-                        message_text: `<b>${movie.title}</b>` +
-                        `\n⭐ ${movie.popularity}` +
-                        `\n📅 ${movie.release_date}` +
-                        `\n🌐 ${movie.original_language}` +
-                        `\n🎭 ${movie.genre_ids}` +
-                        `\n\n${movie.overview}\n` +
-                        `\n <a href="http://image.tmdb.org/t/p/original${movie.poster_path}">&#160;</a>`,
-                        parse_mode: 'HTML',
-                        disable_web_page_preview: false
-                    }
+bot.on('inline_query', async ({inlineQuery, answerInlineQuery}) => {
+    if (inlineQuery.query !== '') {
+        const b = await fetchData(inlineQuery.query);
+        const results = b.results.map((movie) => {
+            return {
+                type: 'article',
+                id: movie.id,
+                title: movie.title,
+                description: movie.overview,
+                thumb_url: `http://image.tmdb.org/t/p/original${movie.poster_path}`,
+                input_message_content: {
+                    message_text: `<b>${movie.title}</b>` +
+                    `\n⭐ ${movie.popularity}` +
+                    `\n📅 ${movie.release_date}` +
+                    `\n🌐 ${movie.original_language}` +
+                    `\n🎭 ${movie.genre_ids}` +
+                    `\n\n${movie.overview}\n` +
+                    `\n <a href="http://image.tmdb.org/t/p/original${movie.poster_path}">&#160;</a>`,
+                    parse_mode: 'HTML',
+                    disable_web_page_preview: false
                 }
-            });
-            return ctx.answerInlineQuery(results, {
-                cache_time: 0
-            })
+            }
         });
+        return answerInlineQuery(results)
+    }
+    /* fetchData(ctx.inlineQuery.query, (b) => {
+         const results = b.results.map((movie) => {
+             return {
+                 type: 'article',
+                 id: movie.id,
+                 title: movie.title,
+                 description: movie.overview,
+                 thumb_url: `http://image.tmdb.org/t/p/original${movie.poster_path}`,
+                 input_message_content: {
+                     message_text: `<b>${movie.title}</b>` +
+                     `\n⭐ ${movie.popularity}` +
+                     `\n📅 ${movie.release_date}` +
+                     `\n🌐 ${movie.original_language}` +
+                     `\n🎭 ${movie.genre_ids}` +
+                     `\n\n${movie.overview}\n` +
+                     `\n <a href="http://image.tmdb.org/t/p/original${movie.poster_path}">&#160;</a>`,
+                     parse_mode: 'HTML',
+                     disable_web_page_preview: false
+                 }
+             }
+         });
+         return ctx.answerInlineQuery(results, {
+             cache_time: 0
+         })
+     });*/
     else {
-        return ctx.answerInlineQuery()
+        return answerInlineQuery()
     }
 });
 
